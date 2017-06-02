026900fe21815013066d4ec5b2921232794c42fe
mariofdesousa <mariofdesousa@gmail.com>
2017-06-02 14:09:38 -0300

    Este arquivo contem algumas funcoes para fazer um cleaning nos textos

---
+++ clean_corpus.R
@@ -0,0 +1,24 @@
+tryTolower <- function(x){
+  # return NA when there is an error
+  y = NA
+  # tryCatch error
+  try_error = tryCatch(tolower(x), error = function(e) e)
+  # if not an error
+  if (!inherits(try_error, 'error'))
+    y = tolower(x)
+  return(y)
+}
+
+
+
+clean.corpus<-function(corpus){
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, stripWhitespace)
+  corpus <- tm_map(corpus, removeNumbers)
+  corpus <- tm_map(corpus, content_transformer(tryTolower))
+  corpus <- tm_map(corpus, removeWords, custom.stopwords)
+  return(corpus)
+}
+
+#Create custom stop words
+custom.stopwords <- c(stopwords('english'), 'lol', 'smh')
